0670518c56b2af16b65c18bc1988396cb6e5dbe3
alexandrejc <alexandrejccruz@gmail.com>
2020-11-30 14:26:09 -0300

    likes do facebook criado.

--- codewears/1.js
+++ codewears/1.js
@@ -1,9 +1,4 @@
 class Executar {
-  constructor () {
-    window.onload = function () {
-      document.getElementById("ini").innerHTML = "<h1>De uma olhada nos metodos e adicione-os ao construtor<h1>";
-    };
-  }
   //Seleciona os numeros pares ou impares de num e
   //mostra os numeros pares ou impares que
   //estão em numero menor
@@ -22,7 +17,34 @@ class Executar {
        if (par.length < impar.length) return par;
        if (par.length > impar.length) return impar;
   }
+  //Semelhante ao likes do facebook, mostra
+  //o numero de likes dentro de um array;
+  likes (curtidas) {
+      let verdadeiro = true;
+      switch (verdadeiro) {
+        case ((curtidas.length - 1) === -1):
+          return "no one like this";
+          break;
+        
+        case ((curtidas.length -1) === 0):
+          return curtidas[0] + " like this";
+          break;
+        
+        case ((curtidas.length -1) === 1):
+          return curtidas[0] + " and " + curtidas[1] + " like this";
+          break;
+       
+       case ((curtidas.length -1) === 2):
+         return curtidas[0] + ", " + curtidas[1] + " and " + curtidas[2] + " like this";
+         break;
+       case ((curtidas.length -1) >= 3):
+         return curtidas[0] + ", " + curtidas[1] + " and " + (curtidas.length - 2) + " others like this";
+         break;
+      }
+   }
+}
+window.onload = function () {
   
+  document.getElementById("ini").innerHTML = "<h1>adicione o metodo que quiser aqui!!!!";
 }
 
-var codigos = new Executar ();
